f1bf2797ee6dfd33ded4cd8db9d7dbdf74c23eef
DESKTOP-CONG\fire_ <dangquoccong0001@gmail.com>
2021-02-03 21:56:07 +0700

    chạy sai

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import React,{useState} from 'react';
 import {v4 as uuid } from 'uuid';
 import store from './utils/store'
 import StoreApi from './utils/storeApi'
+import InputContainer from './components/Input/InputContainer';
+import {makeStyles} from '@material-ui/core/styles';
+const useStyle = makeStyles((theme) =>({
+    root: {
+       display: 'flex',
+       minHeight:'100vh',
+       background:'green',
+    },
+   
+}))
+
 function App() {
+  const classes =useStyle();
   const [data,setData] =useState(store);
   const addMoreCard=(title,listId)=>{
+    console.log(title,listId)
    const newCardId =uuid();
    const newCard={
      id:newCardId,
@@ -21,16 +34,31 @@ function App() {
      },
    };
    setData(newState);
-  }
+  };
+  const addMoreList = (title) => {
+    const newListId = uuid();
+    const newList = {
+      id: newListId,
+      title,
+      cards: [],
+    };
+    const newState = {
+      listIds: [...data.listIds, newListId],
+      lists: {
+        ...data.lists,
+        [newListId]: newList,
+      },
+    };
+    setData(newState);
+  };
   return (
-    <StoreApi.Provider value={{ addMoreCard}}>
-
-    
-    <div>
+    <StoreApi.Provider value={{ addMoreCard,addMoreList}}>
+    <div className={classes.root}>
       {data.listIds.map((listId)=>{
         const list = data.lists[listId];
         return <List list={list} key={listId} /> 
       })}
+      <InputContainer type="text"/>
     </div>
     </StoreApi.Provider>
   );
--- src/components/Input/InputCard.js
+++ src/components/Input/InputCard.js
@@ -1,63 +1,77 @@
-import { Button, IconButton, InputBase, Paper } from '@material-ui/core'
-import React,{useState,useContext} from 'react';
+import React, { useState, useContext } from 'react';
+import { Paper, InputBase, Button, IconButton } from '@material-ui/core';
 import ClearIcon from '@material-ui/icons/Clear';
-import {fade, makeStyles} from '@material-ui/core/styles';
+import { makeStyles, fade } from '@material-ui/core/styles';
 import storeApi from '../../utils/storeApi';
-const useStyle = makeStyles((theme) =>({
-    card: {
-        margin:theme.spacing(0,1,1,1),
-        paddingBottom:theme.spacing(4),
-    },
-    input:{
-        margin:theme.spacing(1),
-    },
-    btnConfirm:{
-        background:"#5AAC44",
-        color:"#fff",
-        "&:hover":{
-            background:fade("#5AAC44",0.75)
-        }
-    },
-    confirm:{
-        margin:theme.spacing(0,1,1,1),
-    }
-}))
-function InputCard({setOpen,listId}) {
-    const classes =useStyle();
-    const {addMoreCard}=useContext(storeApi);
-    const [cardTitle,setCartTitle]=useState('');
 
-    const handleChange=(e)=>{
-        setCartTitle(e.target.value);
-    };
-    const handleBtnConfirm=(e)=>{
-        addMoreCard(cardTitle,listId);
-        setCartTitle('');
-        setOpen(false);
+const useStyle = makeStyles((theme) => ({
+  card: {
+    margin: theme.spacing(0, 1, 1, 1),
+    paddingBottom: theme.spacing(4),
+  },
+  input: {
+    margin: theme.spacing(1),
+  },
+  btnConfirm: {
+    background: '#5AAC44',
+    color: '#fff',
+    '&:hover': {
+      background: fade('#5AAC44', 0.75),
+    },
+  },
+  confirm: {
+    margin: theme.spacing(0, 1, 1, 1),
+  },
+}));
+export default function InputCard({ setOpen, listId, type }) {
+  const classes = useStyle();
+  const { addMoreCard, addMoreList } = useContext(storeApi);
+  const [title, setTitle] = useState('');
+
+  const handleOnChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleBtnConfirm = () => {
+    if (type === 'card') {
+      addMoreCard(title, listId);
+      setTitle('');
+      setOpen(false);
+    } else {
+      addMoreList(title);
+      setTitle('');
+      setOpen(false);
     }
-    const handleBlur=()=>{
-        setOpen(false);
-        setCartTitle('');
-    }
-    return (
-        <div>
-            <div >
-                <Paper className={classes.card}>
-                    <InputBase value={cardTitle} onChange={handleChange} multiline onBlur={handleBlur} fullWidth inputProps={{
-                        className:classes.input,
-                    }}
-                    placeholder="Enter a title of this card"
-                    />
-                </Paper>
-            </div>
-            <div className={classes.confirm}>
-                <Button className={classes.btnConfirm} onClick={handleBtnConfirm}>add card</Button>
-                <IconButton>
-                    <ClearIcon onClick={()=>setOpen(false)}/>
-                </IconButton>
-            </div>
-        </div>
-    )
+  };
+
+  return (
+    <div>
+      <div>
+        <Paper className={classes.card}>
+          <InputBase
+            onChange={handleOnChange}
+            multiline
+            onBlur={() => setOpen(false)}
+            fullWidth
+            inputProps={{
+              className: classes.input,
+            }}
+            value={title}
+            placeholder={
+              type === 'card'
+                ? 'Enter a title of this card..'
+                : 'Enter list title...'
+            }
+          />
+        </Paper>
+      </div>
+      <div className={classes.confirm}>
+        <Button className={classes.btnConfirm} onClick={handleBtnConfirm}>
+          {type === 'card' ? 'Add Card' : 'Add List'}
+        </Button>
+        <IconButton onClick={() => setOpen(false)}>
+          <ClearIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
 }
-
-export default InputCard
