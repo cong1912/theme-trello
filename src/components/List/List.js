4b0dbf1c9f72071d4478cba7eb52e174bb16e9cf
DESKTOP-CONG\fire_ <dangquoccong0001@gmail.com>
2021-02-03 14:55:24 +0700

    first commit

---
+++ src/components/List/List.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {CssBaseline, Paper, Typography} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles';
+import Title from './Title';
+import Card from '../Card';
+import InputContainer from './../Input/InputContainer'
+
+
+const useStyle = makeStyles((theme) =>({
+    root: {
+        width:'300px',
+        backgroundColor:'#EBECF0',
+        marginLeft:theme.spacing(1),
+    }
+}))
+function List() {
+    const classes =useStyle();
+    return (
+        <div>
+            <Paper className={classes.root}>
+                <CssBaseline/>
+                <Typography>
+                    <Title/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <InputContainer/>
+                </Typography>
+              
+            </Paper>
+        </div>
+    )
+}
+
+export default List
---
+++ src/components/List/Title.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { Typography, InputBase } from "@material-ui/core";
+import {makeStyles} from '@material-ui/core/styles';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+
+const useStyle = makeStyles((theme) =>({
+    root: {
+        width:'300px',
+        backgroundColor:'#EBECF0',
+        marginLeft:theme.spacing(1),
+    },
+    edittableTitle:{
+       flexGrow:1,
+       fontsize:'1.2rem',
+       fontWeight:'bold'
+    },
+    edittableTitleContainer:{
+        marginLeft:theme.spacing(1),
+        display: 'flex',
+    },
+    input:{
+        margin:theme.spacing(1),
+        '&:focus':{
+            background:'#ddd'
+        }
+    }
+}))
+function Title() {
+    const [open, setOpen] = useState(false);
+  const classes=useStyle();
+  return (
+    <div>
+      {open ? (
+        <div>
+          <InputBase autoFocus onBlur={()=>setOpen(!open)} fullWidth value="todo" inputProps={{className:classes.input,}} />
+        </div>
+      ) : (
+        <div className={classes.edittableTitleContainer}>
+          <Typography className={classes.edittableTitle} onClick={()=>setOpen(!open)}>Todo</Typography>
+            <MoreHorizIcon/>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Title;
