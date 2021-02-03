3ee58c877291e777d623f18e16683e77e059a994
DESKTOP-CONG\fire_ <dangquoccong0001@gmail.com>
2021-02-03 22:02:24 +0700

    dung add list add card

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
+import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 import List from './components/List/List';
-import React,{useState} from 'react';
-import {v4 as uuid } from 'uuid';
-import store from './utils/store'
-import StoreApi from './utils/storeApi'
+import store from './utils/store';
+import StoreApi from './utils/storeApi';
 import InputContainer from './components/Input/InputContainer';
-import {makeStyles} from '@material-ui/core/styles';
-const useStyle = makeStyles((theme) =>({
-    root: {
-       display: 'flex',
-       minHeight:'100vh',
-       background:'green',
-    },
-   
-}))
+import { makeStyles } from '@material-ui/core/styles';
 
-function App() {
-  const classes =useStyle();
-  const [data,setData] =useState(store);
-  const addMoreCard=(title,listId)=>{
-    console.log(title,listId)
-   const newCardId =uuid();
-   const newCard={
-     id:newCardId,
-     title,
-   };
-   const list = data.lists[listId];
-   list.cards=[...list.cards,newCard]
+const useStyle = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    minHeight: '100vh',
+    background: 'green',
+  },
+}));
 
-   const newState ={
-     ...data,lists:{
-       ...data.list,
-       [listId]:list,
-     },
-   };
-   setData(newState);
+export default function App() {
+  const [data, setData] = useState(store);
+  const classes = useStyle();
+  const addMoreCard = (title, listId) => {
+    console.log(title, listId);
+
+    const newCardId = uuid();
+    const newCard = {
+      id: newCardId,
+      title,
+    };
+
+    const list = data.lists[listId];
+    list.cards = [...list.cards, newCard];
+
+    const newState = {
+      ...data,
+      lists: {
+        ...data.lists,
+        [listId]: list,
+      },
+    };
+    setData(newState);
   };
+
   const addMoreList = (title) => {
     const newListId = uuid();
     const newList = {
@@ -52,16 +56,14 @@ function App() {
     setData(newState);
   };
   return (
-    <StoreApi.Provider value={{ addMoreCard,addMoreList}}>
-    <div className={classes.root}>
-      {data.listIds.map((listId)=>{
-        const list = data.lists[listId];
-        return <List list={list} key={listId} /> 
-      })}
-      <InputContainer type="text"/>
-    </div>
+    <StoreApi.Provider value={{ addMoreCard, addMoreList }}>
+      <div className={classes.root}>
+        {data.listIds.map((listId) => {
+          const list = data.lists[listId];
+          return <List list={list} key={listId} />;
+        })}
+        <InputContainer type="list" />
+      </div>
     </StoreApi.Provider>
   );
 }
-
-export default App;
